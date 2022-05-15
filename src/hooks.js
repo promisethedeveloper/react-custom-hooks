1bb53c5d47679746987e176e60edc981e930abd7
Promise Morka <onyekamorka06@gmail.com>
2022-05-15 16:41:19 -0400

    Initial commit

---
+++ src/hooks.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const useFlip = (initialFlipState = true) => {
+	const [isFacingUp, setIsFacingUp] = useState(initialFlipState);
+	const flipCard = () => {
+		setIsFacingUp((isUp) => !isUp);
+	};
+	return [isFacingUp, flipCard];
+};
+
+const useAxios = (keyInLS, baseUrl) => {
+	const [responses, setResponses] = useLocalStorage(keyInLS);
+
+	const addResponseData = async (
+		formatter = (data) => data,
+		restOfUrl = ""
+	) => {
+		const response = await axios.get(`${baseUrl}${restOfUrl}`);
+		setResponses((data) => [...data, formatter(response.data)]);
+	};
+
+	const clearResponses = () => setResponses([]);
+
+	return [responses, addResponseData, clearResponses];
+};
+
+const useLocalStorage = (key, initialValue = []) => {
+	if (localStorage.getItem(key)) {
+		initialValue = JSON.parse(localStorage.getItem(key));
+	}
+	const [value, setValue] = useState(initialValue);
+
+	useEffect(() => {
+		localStorage.setItem(key, JSON.stringify(value));
+	}, [value, key]);
+
+	return [value, setValue];
+};
+
+export default useLocalStorage;
+
+export { useFlip, useAxios, useLocalStorage };
